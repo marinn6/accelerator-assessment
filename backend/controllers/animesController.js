663ffbca2fc192f35b1895fb40410734feaac25f
marinn6 <nmarin@pursuit.org>
2024-08-14 15:36:38 -0400

    assessment

--- backend/controllers/animesController.js
+++ backend/controllers/animesController.js
@@ -24,6 +24,32 @@ const {
 //   }
 // ]
 
+animes.get('/', async (req,res) => {
+  try {
+    const animes = await getAllAnimes()
+    if(animes[0]){
+      res.status(200).json(animes)
+    } else {
+      console.error(err)
+      res.status(404).json({ error: "No Animes Found"})
+    }
+  } catch(error){
+    console.error(error)
+    res.status(500).json({ error: 'An internal server error occured while retrieving animes.' })
+  }
+})
+
+animes.get('/:id', async (req, res) => {
+  const { id } = req.params
+  const anime = await getOneAnime(id)
+  if(id){
+    res.status(200).json(anime)
+  } else {
+    res.status(404).json({ error: "Anime Not Found"})
+  }
+});
+
+
 //Write a POST route that takes user provided data from the request body and creates a new anime in the database. The route should respond with a 201 status code and the new anime.
 //if the request body does not contain a name and description, or if the body's name or description have no length, respond with an error
 //your response body should look this:
@@ -33,14 +59,32 @@ const {
 //   "description": "this is anime"
 // }
 
-//Write a PUT route that takes user provided data from the request body and updates an existing anime in the database. The route should respond with a 200 and the updated anime. The route should be able to handle a non-existent anime id.
-//if the request body does not contain a name and description, or if the body's name or description have no length, respond with an error
-//your response body should look this:
+animes.post('/', async (req, res) => {
+  const { name, description } = req.body
+  const newAnime = await createOneAnime(name, description)
+  res.status(201).json(newAnime)
+});
+
+
+// Write a PUT route that takes user provided data from the request body and updates an existing anime in the database. The route should respond with a 200 and the updated anime. The route should be able to handle a non-existent anime id.
+// if the request body does not contain a name and description, or if the body's name or description have no length, respond with an error
+// your response body should look this:
 // {
 //   "id": 20,
 //   "name": "test1",
 //   "description": "this is anime as well"
 // }
+
+animes.put('/:id', async (req, res) => {
+  const{ id } = req.params
+  const updatedAnime = await updateOneAnime(id, req.body)
+  if(updatedAnime.id){
+    res.status(200).json(updatedAnime)
+  } else {
+    req.status(500).json({ error: "Anime Not Found"})
+  }
+});
+
 
 //Write a DELETE route that deletes a single anime by id (provided by the client as a request param) from the database and responds with a 200 and the deleted anime data. The route should be able to handle a non-existent anime id.
 //your response body should look this:
@@ -49,4 +93,21 @@ const {
 //   "name": "test1",
 //   "description": "this is anime as well"
 // }
+
+animes.delete('/:id', async (req, res) => {
+  try {
+    const { id } = req.params
+    const deletedAnime = await deleteOneAnime(id)
+
+    if(deletedAnime.id){
+      res.status(200).json({ sucess: "Successfully deleted task"})
+    } else {
+      req.status(404).json({ error: "Task not found"})
+    }
+  } catch (error) {
+    res.status(400).json({ error: error})
+  }
+})
+
+
 module.exports = animes;
--- frontend/src/Components/Animes.js
+++ frontend/src/Components/Animes.js
@@ -1,10 +1,29 @@
 import Anime from "./Anime";
+import React, { useEffect, useState } from "react"
+
 
 function Animes() {
+  const API = process.env.REACT_APP_BASE_URL;
+  const [animes, setAnimes] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:3001/animes`)
+    .then(res => res.json() )
+    .then(res => {
+      setAnimes(res)
+  })
+  .catch(err => console.error(err))
+}, [])
   //fetch all animes from the backend and render them as a list using the Anime component. Make sure to style the animes to look like the screenshot from the README. Feel free to use axios to grab data
 
   return (
     <section className="index" id="anime-list">
+      { animes.map((anime, index) => {
+        const {name, description} = Anime
+        return (
+          //I didnt get to finish this part
+        )
+      })}
       <p>No animes to display</p>
     </section>
   );
